7e0cf4e005479074a682133a87c5d69d49ae4554
Duy <duytk0022@gmail.com>
2023-09-06 19:12:42 +0700

    Sort

---
+++ src/main/java/View/Main.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package View;
+import Common.Library;
+import Model.Element;
+import Controller.SortPrograming;
+/**
+ *
+ * @author PC
+ */
+public class Main {
+    public static void main(String[] args) {
+        Library library = new Library();
+        Element element = new Element();
+        //1. Enter a possitive number
+        element.setSize_array(library.getInt("Enter number of array", 1, 100));
+        //2. Create array by generate random integer in range
+        element.setArray(library.createArray(element.size_array));
+        new SortPrograming(element).run();
+    }
+}
